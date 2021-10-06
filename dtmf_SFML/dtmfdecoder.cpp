3a545624080625f2e8f6030b6069e0de805e2a76
Sigurd Jager Lemming <sigurdjagerlemming@gmail.com>
2021-10-06 12:03:06 +0200

    fft, recorddata og modulus funktioner i klasse

--- dtmf_SFML/dtmfdecoder.cpp
+++ dtmf_SFML/dtmfdecoder.cpp
@@ -1,1 +1,93 @@
 #include "dtmfdecoder.h"
+#include <complex>
+#include <SFML/Audio.hpp>
+#include <cmath>
+
+DtmfDecoder::DtmfDecoder(){
+
+}
+
+int DtmfDecoder::doSample(int msDuration)
+{
+
+}
+
+vector<complex<double>> DtmfDecoder::recordData(int msDuration)
+{
+    sf::Clock clock;
+    _recorder.start();
+
+    while(clock.getElapsedTime().asMilliseconds() < msDuration){}
+
+    _recorder.stop();
+
+    const sf::SoundBuffer& buffer = _recorder.getBuffer();
+
+    const sf::Int16* samples = buffer.getSamples();
+    const long count = buffer.getSampleCount();
+
+    vector<complex<double>> data;
+    for (int i = 0; i < count; i++){
+        complex<double> number;
+        number.real((double)samples[i]);
+        data.push_back(number);
+    }
+    return data;
+}
+
+vector<complex<double>> DtmfDecoder::fft(vector<complex<double> > &a)
+{
+    // https://www.geeksforgeeks.org/fast-fourier-transformation-poynomial-multiplication/
+
+    using cd = complex<double>;
+    int n = a.size();
+
+    // if input contains just one element
+    if (n == 1)
+        return vector<cd>(1, a[0]);
+
+    // For storing n complex nth roots of unity
+    vector<cd> w(n);
+    for (int i = 0; i < n; i++) {
+        double alpha = -2 * M_PI * i / n;
+        w[i] = cd(cos(alpha), sin(alpha));
+    }
+
+    vector<cd> A0(n / 2), A1(n / 2);
+    for (int i = 0; i < n / 2; i++) {
+
+        // even indexed coefficients
+        A0[i] = a[i * 2];
+
+        // odd indexed coefficients
+        A1[i] = a[i * 2 + 1];
+    }
+
+    // Recursive call for even indexed coefficients
+    vector<cd> y0 = fft(A0);
+
+    // Recursive call for odd indexed coefficients
+    vector<cd> y1 = fft(A1);
+
+    // for storing values of y0, y1, y2, ..., yn-1.
+    vector<cd> y(n);
+
+    for (int k = 0; k < n / 2; k++) {
+        y[k] = y0[k] + w[k] * y1[k];
+        y[k + n / 2] = y0[k] - w[k] * y1[k];
+    }
+    return y;
+}
+
+vector<double> DtmfDecoder::modulus(const vector<complex<double>>& data)
+{
+    vector<double> ampVector;
+
+    for (int i = _cutoffLow; i < _cutoffHigh; i++){
+        double real = data.at(i).real();
+        double imag = data.at(i).imag();
+        double amp = sqrt(real*real + imag*imag);
+        ampVector.push_back(amp);
+    }
+    return ampVector;
+}
--- dtmf_SFML/dtmfdecoder.h
+++ dtmf_SFML/dtmfdecoder.h
@@ -9,18 +9,22 @@ using namespace std;
 class DtmfDecoder
 {
 public:
-    DtmfDecoder(int dataRate);
-    int doSample(int duration);
+    DtmfDecoder();
+    int doSample(int msDuration);
 
 private:
     const int _lowFreqs[4] = {697, 770, 852, 941};
     const int _highFreqs[4] = {1209, 1336, 1477, 1633};
+    const int _cutoffLow = 600;
+    const int _cutoffHigh = 3000;
 
-    vector<complex<double>> recordData();
-    void fft(vector<complex<double>>& data);
-    vector<double> modulus(const complex<double>& data);
-    // identifyPeaks(vector<double>& data)
-    // identifyClosestDtmf(peaks&)
+    vector<complex<double>> recordData(int msDuration);
+    vector<complex<double>> fft(vector<complex<double>>& data);
+    vector<double> modulus(const vector<complex<double>>& data);
+
+    // Find closest dtmf tone
+    int splitHighestPeak(const vector<double>& data);
+    int sampleAtFreq(const vector<double>& data);
 
     sf::SoundBufferRecorder _recorder;
 };
