859a7e480f2cc38d8fec3d7ebd2061803d4d19c8
phsan20 <phsan20@student.sdu.dk>
2021-10-06 15:31:33 +0200

    Inttobool working, not tested

--- Data Transfer Layer/Protocol/bytestuffer.cpp
+++ Data Transfer Layer/Protocol/bytestuffer.cpp
@@ -8,47 +8,51 @@ bytestuffer::bytestuffer(vector<bool> flag,vector<bool> etc){
     _flag=flag;
     _etcI=boolToInt(etc).at(0);
     _flagI=boolToInt(flag).at(0);
-
 }
 
 bytestuffer::~bytestuffer(){};
 
-vector<int> bytestuffer::boolToInt(vector<bool> in){
-vector<int> out;
-while(in.size()>0){
-    out.push_back(in.at(0)*1+in.at(1)*2+in.at(2)*4+in.at(3)*8); //4 bits til "hex" som int
-}
-return out;
+vector<int> bytestuffer::boolToInt(vector<bool> in) {
+    vector<int> out;
+    while(in.size()>0){
+        out.push_back(in.at(0)*8+in.at(1)*4+in.at(2)*2+in.at(3)*1); //4 bits til "hex" som int
+        in.erase(in.begin());//4 bits deleted
+        in.erase(in.begin());
+        in.erase(in.begin());
+        in.erase(in.begin());
+    }
+
+    return out;
 }
 
 vector<bool> bytestuffer::intToBool(vector<int> in){
-vector<bool> out;
-for(int i=0;i<in.size();i++){
-    int testInt=in.at(i);
-    if(testInt>=8){ //if int>8, first bit needs to be set
-        testInt=testInt%8;//modulo gives the remainder
-        out.push_back(1);
-    } else {
-        out.push_back(0); //if int<8, bit is not set
+    vector<bool> out;
+    for(int i=0;i<in.size();i++){
+        int testInt=in.at(i);
+        if(testInt>=8){ //if int>8, first bit needs to be set
+            testInt=testInt%8;//modulo gives the remainder
+            out.push_back(1);
+        } else {
+            out.push_back(0); //if int<8, bit is not set
+        }
+        if(testInt>=4){
+            testInt=testInt%4;
+            out.push_back(1);
+        } else {
+            out.push_back(0);
+        }
+        if(testInt>=2){
+            testInt=testInt%2;
+            out.push_back(1);
+        } else {
+            out.push_back(0);
+        }
+        if(testInt>=1){
+            out.push_back(1);
+        } else {
+            out.push_back(0);
+        }
     }
-    if(testInt>=4){
-        testInt=testInt%4;
-        out.push_back(1);
-    } else {
-        out.push_back(0);
-    }
-    if(testInt>=2){
-        testInt=testInt%2;
-        out.push_back(1);
-    } else {
-        out.push_back(0);
-    }
-    if(testInt>=1){
-        out.push_back(1);
-    } else {
-        out.push_back(0);
-    }
-}
-return out;
+    return out;
 }    
 
--- Data Transfer Layer/Protocol/main.cpp
+++ Data Transfer Layer/Protocol/main.cpp
@@ -4,9 +4,23 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
+
     bytestuffer stuffer1({1,1,1,1},{0,0,0,0});
     vector<bool> bools={1,0,1,1,1,0,0,0,0,1,1,1};
+    cout <<" bools" << endl;
+    for(bool i:bools){
+        cout<<i;
+    }
+    cout << endl;
+    cout << "ints" << endl;
+    vector<int> inter;
     for(int i:stuffer1.boolToInt(bools)){
+        inter.push_back(i);
+        cout <<" "<< i;
+    }
+    cout << endl;
+        cout << "bools" << endl;
+    for(int i:stuffer1.intToBool(inter)){
         cout << i;
     }
     cout << endl;
