2ab090561358c57b552d261b088c311990d7a6e2
Sigurd Jager Lemming <sigurdjagerlemming@gmail.com>
2021-10-31 15:16:01 +0100

    Basic threads and mutex locks

--- dtmf_SFML/physicallayer.cpp
+++ dtmf_SFML/physicallayer.cpp
@@ -1,6 +1,52 @@
 #include "physicallayer.h"
+#include <iostream>
+#include <thread>
+#include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <vector>
 
-PhysicalLayer::PhysicalLayer(double duration, double spacing)
+using namespace std;
+
+PhysicalLayer::PhysicalLayer(double duration)
 {
+    sem_init(&_inBufferMutex, 0, 1);
+    sem_init(&_outBufferMutex, 0, 1);
+    new thread(&PhysicalLayer::encoding, this);
+    new thread(&PhysicalLayer::decoding, this);
+}
 
+void PhysicalLayer::encoding()
+{
+    cout << "encoding called" << endl;
+
+    while(true){
+        sleep(1);
+        if (_outBuffer.size() > 0){
+            sem_wait(&_outBufferMutex);
+            cout << _outBuffer.at(0) << endl;
+            _outBuffer.erase(_outBuffer.begin());
+            sem_post(&_outBufferMutex);
+        }
+
+    }
+
+    return;
+}
+
+void PhysicalLayer::decoding()
+{
+    cout << "decoding called" << endl;
+
+    int i = 0;
+    while(true){
+        sleep(1);
+        sem_wait(&_outBufferMutex);
+        _outBuffer.push_back(i);
+        sem_post(&_outBufferMutex);
+
+        i++;
+    }
+
+    return;
 }
--- dtmf_SFML/physicallayer.h
+++ dtmf_SFML/physicallayer.h
@@ -1,24 +1,25 @@
 #ifndef PHYSICALLAYER_H
 #define PHYSICALLAYER_H
 #include <vector>
+#include <semaphore.h>
 
 using namespace std;
 
 class PhysicalLayer
 {
 public:
-    PhysicalLayer(double duration, double spacing);
+    PhysicalLayer(double duration);
     bool readInBuffer(int& dtmf);
     bool writeOutBuffer(int dtmf);
 
 private:
-    vector<int> _outBuffer;
-    vector<int> _inBuffer;
+    vector<int> _outBuffer, _inBuffer;
     double _duration;
-    double _spacing;
 
-    void encoding(vector<int>& _outBuffer);
-    void decoding(vector<int>& _inBuffer);
+    sem_t _inBufferMutex, _outBufferMutex;
+
+    void encoding();
+    void decoding();
 };
 
 #endif // PHYSICALLAYER_H
