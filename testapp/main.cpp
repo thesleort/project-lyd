1ecf417e570df8f466c1082e71e5b08bfa285e51
Troels Blicher Petersen <troels@newtec.dk>
2021-11-11 00:14:56 +0100

    testapp: Fixed socket2 receive loop

--- testapp/main.cpp
+++ testapp/main.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <vector>
 #include <iostream>
+#include <atomic>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -13,23 +14,27 @@
 
 #include "libdtmf.h"
 
-void socket1(DTMF*dtmfClass);
-void socket2(DTMF*dtmfClass);
+void socket1(DTMF*dtmfClass, std::atomic<bool> &runFlag);
+void socket2(DTMF*dtmfClass, std::atomic<bool> &runFlag);
 
 int main(int argc, char *argv[]) {
+  std::atomic<bool> runFlag(true);
   std::vector<DTMFFrame> buffer1;
   std::vector<DTMFFrame> buffer2;
   DTMF *dtmfSocket1 = new DTMF(&buffer1, &buffer2);
   DTMF *dtmfSocket2 = new DTMF(&buffer2, &buffer1);
 
-  std::thread socketThread1(&socket1, dtmfSocket1);
-  std::thread socketThread2(&socket2, dtmfSocket2);
+  std::thread socketThread1(&socket1, dtmfSocket1, std::ref(runFlag));
+  std::thread socketThread2(&socket2, dtmfSocket2, std::ref(runFlag));
   do {
    std::cout << '\n' << "Press a key to close...";
   } while (std::cin.get() != '\n');
+  runFlag = false;
+  socketThread1.join();
+  socketThread2.join();
 }
 
-void socket1(DTMF *dtmfClass) {
+void socket1(DTMF *dtmfClass, std::atomic<bool> &runFlag) {
   std::cout << "Creating socket1" << std::endl;
   DTMFFrame frame;
   frame.sizeBytes = 10;
@@ -39,12 +44,13 @@ void socket1(DTMF *dtmfClass) {
   std::cout << "Socket 1: frame sent (" << frame.sizeBytes << ")" << std::endl;
 }
 
-void socket2(DTMF *dtmfClass) {
+void socket2(DTMF *dtmfClass, std::atomic<bool> &runFlag) {
   std::cout << "Creating socket2" << std::endl;
-  DTMFFrame frame;
-  while (true) {
-    dtmfClass->receive(frame);
-
-    std::cout << "Socket 2: frame received (" << frame.sizeBytes << ")" << std::endl;
+  DTMFFrame *frame = new DTMFFrame;
+  while (runFlag) {
+    if (dtmfClass->receive(*frame, false) > 0) {
+      std::cout << "Socket 2: frame received (" << frame->sizeBytes << ")" << std::endl;
+    }
   }
+  delete frame;
 }
