093556d86f91a0c9716068cad61c7837ece4dc05
Troels Blicher Petersen <troels@newtec.dk>
2021-11-11 12:38:18 +0100

    masterapp: Created initial masterapp program and setup

---
+++ masterapp/create-master-app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Prepare libdtmf library
+cd ../lib
+mkdir -p build
+cd build
+cmake ..
+make -j$(nproc)
+cp libdtmf.so* ../../masterapp
+cd ..
+cp lib/libdtmf.h ../masterapp
+cd ../masterapp
+
+# Compile master application
+mkdir -p build
+cd build
+cmake ..
+make -j$(nproc)
