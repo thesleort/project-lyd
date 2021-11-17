30dc22562fccb481adb8a09ef28c99087a496bc1
Troels Blicher Petersen <troels@newtec.dk>
2021-11-17 13:19:31 +0100

    made packet.h

---
+++ slaveapp/packet.h
@@ -0,0 +1,14 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#define DATATYPE_MOVE   0b10000000
+
+#define MOVE_STOP       0b00000000
+#define MOVE_FORWARD    0b00000001
+#define MOVE_BACKWARDS  0b00000010
+#define MOVE_RIGHT90    0b00000100
+#define MOVE_LEFT90     0b00001000
+
+// Ved DATATYPE_MOVE skal den 'or' med en af MOVE_ definitionerne
+
+#endif
