30364afc48438f94f243e9074b54e0c83c5935cf
Troels Blicher Petersen <troels@newtec.dk>
2021-12-02 15:57:13 +0100

    libdtmf: test

--- masterapp/main.cpp
+++ masterapp/main.cpp
@@ -16,61 +16,62 @@ int main(void) {
   frame.data = new uint8_t(frame.data_size);
 
   frame.data[0] = DATATYPE_MOVE | MOVE_FORWARD;
+  dtmf->transmit(frame);
   // dtmf->transmit(frame);
-  // while(true) {
+  while(true) {
 
-  // }
-  initscr();
-
-  cbreak();
-  noecho();
-  nodelay(stdscr, TRUE);
-  scrollok(stdscr, TRUE);
-
-  int flag;
-
-  std::cout << "Press any key to start" << std::endl;
-  flag = std::cin.get();
-
-  bool inputLoop = true;
-  while (inputLoop) {
-
-    
-    int c = getch();
-    clear();
-    
-    switch (c) {
-    case 119:
-      frame.data[0] = DATATYPE_MOVE | MOVE_FORWARD;
-      printw("w");
-      break;
-    case 115:
-      frame.data[0] = DATATYPE_MOVE | MOVE_BACKWARDS;
-      printw("s");
-      break;
-    case 97:
-      frame.data[0] = DATATYPE_MOVE | MOVE_LEFT90;
-      printw("a");
-      break;
-    case 100:
-      frame.data[0] = DATATYPE_MOVE | MOVE_RIGHT90;
-      printw("d");
-      break;
-    case 32:
-      frame.data[0] = DATATYPE_MOVE | MOVE_STOP;
-      printw("spacebar");
-      break;
-    case 27:
-      inputLoop = false;
-      endwin();
-      std::cout << "Program ended"<< std::endl;
-      break;
-    default:
-      break;
-    };
-
-    dtmf->transmit(frame);
   }
+  // initscr();
+
+  // cbreak();
+  // noecho();
+  // nodelay(stdscr, TRUE);
+  // scrollok(stdscr, TRUE);
+
+  // int flag;
+
+  // std::cout << "Press any key to start" << std::endl;
+  // flag = std::cin.get();
+
+  // bool inputLoop = true;
+  // while (inputLoop) {
+
+    
+  //   int c = getch();
+  //   clear();
+    
+  //   switch (c) {
+  //   case 119:
+  //     frame.data[0] = DATATYPE_MOVE | MOVE_FORWARD;
+  //     printw("w");
+  //     break;
+  //   case 115:
+  //     frame.data[0] = DATATYPE_MOVE | MOVE_BACKWARDS;
+  //     printw("s");
+  //     break;
+  //   case 97:
+  //     frame.data[0] = DATATYPE_MOVE | MOVE_LEFT90;
+  //     printw("a");
+  //     break;
+  //   case 100:
+  //     frame.data[0] = DATATYPE_MOVE | MOVE_RIGHT90;
+  //     printw("d");
+  //     break;
+  //   case 32:
+  //     frame.data[0] = DATATYPE_MOVE | MOVE_STOP;
+  //     printw("spacebar");
+  //     break;
+  //   case 27:
+  //     inputLoop = false;
+  //     endwin();
+  //     std::cout << "Program ended"<< std::endl;
+  //     break;
+  //   default:
+  //     break;
+  //   };
+
+  //   dtmf->transmit(frame);
+  // }
 
   return 0;
 }
